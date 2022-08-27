0e57b75ace2491e1879db523846cadcb97562d65
Dayane Chiozzi <daychiozzi@gmail.com>
2022-08-26 23:10:01 -0300

    :sparkles:feat:migration & model Categories done.

---
+++ src/database/models/category.js
@@ -0,0 +1,20 @@
+const Category = (Sequelize, DataTypes) => {
+  Category = Sequelize.define('Categories', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+  },
+    {
+      tableName: 'categories',
+      timestamps: false,
+      underscored: true,
+    });
+
+  return Category;
+}
